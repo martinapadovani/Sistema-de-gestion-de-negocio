8b6cfd673759807033021e4afab137fc9e70641c
TZeeTZzz <franciscoadoue@gmail.com>
2023-07-06 10:18:56 -0300

    fix: borrar rama de prueba

--- ProyectoFinalJava/src/ClasesConcretas/Gasto.java
+++ ProyectoFinalJava/src/ClasesConcretas/Gasto.java
@@ -29,7 +29,6 @@ public class Gasto extends Transaccion implements GestionDeFacturas<Gasto>{
 	
 	/* Inicializo las variables globalmente. Estas van a ser rellenadas con lo que se obtenga 
 	 * de la base de datos.*/
-	String stringPrueba = "probando";
 	int idTransaccion = 0;
 	Date fechaDeTransaccion = null;
 	String medioDePago = null;
