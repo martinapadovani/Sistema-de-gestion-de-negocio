4080f1da1c453bf6bc25d8a7da9c5fec5949e712
Martina Padovani <133980405+martinapadovani@users.noreply.github.com>
2023-07-07 10:05:10 -0300

    Update Proveedor.java
    Actualizacion metodos ver, buscar y agregar

--- ProyectoFinalJava/src/ClasesConcretas/Proveedor.java
+++ ProyectoFinalJava/src/ClasesConcretas/Proveedor.java
@@ -10,34 +10,72 @@ public class Proveedor implements GestionDeDatos<Proveedor>{
 	private int id;
 	private String nombreProveedor;
 	private int telefono;
-	private ArrayList<Producto>productosProveedor;
+	//private ArrayList<Producto>productosProveedor;
+	//El dato de a que proveedor corresponde cada producto va a ser indicado y subido a al DB desde la clase de productos, con la llave foranea
+	
+	//ATRIBUTOS PARA CONEXION
+	Conexion conexion = new Conexion();
+	private Connection cn = null;
+	
+	//SCANNER
+	Scanner scanner = new Scanner (System.in);
 	
 	//CONSTRUCTOR
-	
 	public Proveedor(int id, String nombreProveedor, int telefono, ArrayList<Producto>productosProveedor) {
 		super();
 		this.id = id;
 		this.nombreProveedor = nombreProveedor;
 		this.telefono = telefono;
-		this.productosProveedor = productosProveedor;
+	//	this.productosProveedor = productosProveedor;
 	}
 	
 	
-	@Override
-	public String toString() {
-		return "Proveedor [id=" + id + ", nombreProveedor=" + nombreProveedor + ", telefono=" + telefono
-				+ ", productosProveedor=" + productosProveedor + "]";
-	}
 
-	
 	//OVERRIDES
-
-	@Override
-	public void Agregar(Proveedor proveedor) {
-	}
 	
+	@Override
+	public void Ver() {
+		
+		try{
+			cn = conexion.conectar();
+			
+			String query = "SELECT * FROM proveedor";
+			
+			Statement declaracion = cn.createStatement();
+			ResultSet resultados = declaracion.executeQuery(query);
+			
+			while(resultados.next()) { //mientras haya datos por leer
+			System.out.println(
+						"ID: " + resultados.getInt("idProveedor") + ". Nombre: " +resultados.getString("nombreProveedor") + 
+						 ". Telefono: " + resultados.getInt("telefonoProveedor"));
+			}
+		} catch(SQLException e){
+			e.printStackTrace();
+		} 
+	}
+
 	@Override
 	public void Buscar(int ID) {
+		
+		try{
+			cn = conexion.conectar();
+			
+			String query = "SELECT * FROM proveedor WHERE  idProveedor = ?";
+			
+			PreparedStatement declaracion  = cn.prepareStatement(query);
+		
+				declaracion.setInt(1, ID);
+				ResultSet resultados = declaracion.executeQuery();
+				
+			while(resultados.next()) { //mientras haya datos por leer
+					System.out.println(
+								"ID: " + resultados.getInt("idProveedor") + ". Nombre: " +resultados.getString("nombreProveedor") + 
+								 ". Telefono: " + resultados.getInt("telefonoProveedor"));
+			}
+			
+		} catch(SQLException e){
+			e.printStackTrace();
+		} 
 	}
 	
 	@Override
@@ -45,11 +83,47 @@ public class Proveedor implements GestionDeDatos<Proveedor>{
 	}
 	
 	@Override
+	public void Agregar() {
+		
+		System.out.println("Por favor, ingrese los datos correspondientes");
+		System.out.println("Nombre: ");
+		String nombreProveedor = (scanner.nextLine()).trim().replace(" ", "_");
+		System.out.println("Telefono: ");
+		int telefono = scanner.nextInt();
+
+		try{
+			cn = conexion.conectar();
+			
+			String query = "INSERT INTO proveedor (nombreProveedor, telefonoProveedor) VALUES (?, ?)";
+			//excluyo el id ya que es autoincremental
+			
+			PreparedStatement declaracion  = cn.prepareStatement(query);
+			
+				declaracion.setString(1, nombreProveedor);
+				declaracion.setInt(2, telefono);
+				
+				declaracion.executeUpdate();
+				
+				System.out.println("Datos cargados exitosamente!");
+			
+		} catch(SQLException e){
+			e.printStackTrace();
+		} 
+	}
+	
+	@Override
 	public void Eliminar(Proveedor proveedor) {
+	}
+	
+	
+	//TO STRING
+	@Override
+	public String toString() {
+		return "Proveedor: id=" + id + ", nombreProveedor=" + nombreProveedor + ", telefono=" + telefono
+				+ ", productosProveedor=";
 	}
 
 	// GETTERS-SETTERS: 
-
 	public int getId() {
 		return id;
 	}
