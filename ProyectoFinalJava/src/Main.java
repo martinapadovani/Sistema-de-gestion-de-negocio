907b0a1842d59bf2774dcf6a1094371732a12448
Martina Padovani <133980405+martinapadovani@users.noreply.github.com>
2023-07-07 10:00:10 -0300

    Update Main.java
    Interfaz

--- ProyectoFinalJava/src/Main.java
+++ ProyectoFinalJava/src/Main.java
@@ -19,6 +19,267 @@ public class Main {
 
 		venta.mostrarFacturas();
 
+				Scanner scanner = new Scanner (System.in);
+		
+		//boolean salir = false;
+	/*En Java, los parámetros se pasan por valor, si uso este parámetro booleano para el metodo método volverAlMenu
+	se crea una copia del valor original y las modificaciones dentro del método solo afectan a esa copia
+	Para poder actualizar el valor de salir en el bucle while, tengo que usar un objeto mutable, */
+		
+		AtomicBoolean salir = new AtomicBoolean(false);
+	/*La clase AtomicBoolean proporciona métodos para manipular el valor booleano de manera mutable*/
+		
+		while (!salir.get()) {	
+			System.out.println("Bienvenido! Ingrese el sector al que desea acceder:");
+			System.out.println("1. Empleados");
+			System.out.println("2. Proveedores	");
+			System.out.println("3. Productos");
+			System.out.println("4. Ventas");
+			System.out.println("5. Gastos");
+			System.out.println("0. Salir");
+			
+			int sector = scanner.nextInt(); 
+			/* nextInt solo consume el número en sí y deja el carácter de nueva línea (salto de línea) 
+			en el búfer de entrada. Este es capturado por la siguiente llamada a nextLine() y se interpreta como una línea vacía*/
+			
+			   
+			switch(sector) {
+			
+			case 1 : //EMPLEADOS
+				  
+				  System.out.println("Bienvenido al sector de Empleados!");	
+				  System.out.println("Seleccione según corresponda:");
+				  System.out.println("1. Ver empleados");
+				  System.out.println("2. Buscar empleado");
+				  System.out.println("3. Actualizar empleado");
+				  System.out.println("4. Agregar nuevo empleado");
+				  System.out.println("5. Eliminar empleado del sistema");
+				  System.out.println("6. Calcular salario");
+				  System.out.println("7. Calcular desempeño");
+				  
+				  scanner.nextLine(); /* adicional, para consumir el salto de línea pendiente, que queda en el búfer de entrada.
+				   Prepara correctamente el búfer de entrada para que la siguiente llamado, evitando que el nextLine() del titulo, 
+				   lea ese salto de línea (y devuelva una cadena vacía) en lugar de la línea ingresada por el usuario (el título) */
+
+				  
+				  Empleado ejecutarMetodosEmpleado = new Empleado(null, null, 0, 0, null, 0, 0, 0, 0, 0, 0, null, null, false);
+				  int opcionEmpleados = scanner.nextInt(); 
+				  
+			 switch (opcionEmpleados) {
+				 
+				  case 1:
+					  
+					  System.out.println("Elegiste la opcion de ver empleados!");
+					  ejecutarMetodosEmpleado.Ver();
+					  
+					  break;
+				  case 2:
+					  
+					  System.out.println("Elegiste la opcion de buscar empleados!");
+					  
+					  System.out.println("Ingrese el id del Empleado:");
+					  int id = scanner.nextInt(); 
+					  ejecutarMetodosEmpleado.Buscar(id);
+					  
+					  break;
+				  case 3:
+					  
+					  System.out.println("Elegiste la opcion de actualizar empleados!");
+					  ejecutarMetodosEmpleado.Actualizar(ejecutarMetodosEmpleado);
+					  
+					  break;
+				  case 4:
+					  
+					  System.out.println("Elegiste la opcion de agregar empleados!");
+					  ejecutarMetodosEmpleado.Agregar();
+					  
+					  break;
+				  case 5:
+					  
+					  System.out.println("Elegiste la opcion de eliminar empleados!");
+					  ejecutarMetodosEmpleado.Eliminar(ejecutarMetodosEmpleado);
+					  
+					  break;
+			      case 6:
+					  
+					  System.out.println("Elegiste la opcion de calcular salario!");
+					  ejecutarMetodosEmpleado.calcularSalario();
+					  
+					  break;  
+			     case 7:
+				  
+				      System.out.println("Elegiste la opcion de calcular desempeño!");
+				      ejecutarMetodosEmpleado.calcularDesempeño();
+				  
+				  break;
+				 }
+			  volverAlMenu(salir);
+			break;	
+				
+			case 2://PROVEEDORES
+				
+				  System.out.println("Bienvenido al sector de proveedores!");	
+				  System.out.println("Seleccione según corresponda:");
+				  System.out.println("1. Ver proveedores");
+				  System.out.println("2. Buscar proveedor");
+				  System.out.println("3. Actualizar proveedores");
+				  System.out.println("4. Agregar nuevo proveedor");
+				  System.out.println("5. Eliminar proveedor del sistema");
+				  
+				  scanner.nextLine(); /* adicional, para consumir el salto de línea pendiente, que queda en el búfer de entrada.
+				   Prepara correctamente el búfer de entrada para que la siguiente llamado, evitando que el nextLine() del titulo, 
+				   lea ese salto de línea (y devuelva una cadena vacía) en lugar de la línea ingresada por el usuario (el título) */
+				  
+				  Proveedor ejecutarMetodosProveedor = new Proveedor(0, null, 0, null);
+				  int opcionProveedores = scanner.nextInt(); 
+				  
+			 switch (opcionProveedores) {
+			 
+			 case 1:
+				 
+				  System.out.println("Elegiste la opcion de ver proveedores!");
+				  ejecutarMetodosProveedor.Ver();
+				  
+				 break;
+			 case 2:
+				 
+				  System.out.println("Elegiste la opcion de buscar proveedores!");
+				  
+				  System.out.println("Ingrese el id del Proveedor:");
+				  int id = scanner.nextInt(); 
+				  
+				  ejecutarMetodosProveedor.Buscar(id);
+				  
+				 break;
+			 case 3:
+				 
+				  System.out.println("Elegiste la opcion de actualizar empleados!");
+				  ejecutarMetodosProveedor.Actualizar(ejecutarMetodosProveedor);
+				  
+				 break;
+			 case 4:
+				 
+				  System.out.println("Elegiste la opcion de agregar proveedores!");
+				  ejecutarMetodosProveedor.Agregar();
+				  
+				 break;
+			 case 5:
+				 
+				  System.out.println("Elegiste la opcion de eliminar proveedores!");
+				  ejecutarMetodosProveedor.Eliminar(ejecutarMetodosProveedor);
+				  
+				 break;
+			 }
+			  volverAlMenu(salir);
+			   break;
+			
+			case 3://PRODUCTOS
+				
+				  System.out.println("Bienvenido al sector de productos!");	
+				  System.out.println("Seleccione según corresponda:");
+				  System.out.println("1. Ver productos");
+				  System.out.println("2. Buscar producto");
+				  System.out.println("3. Actualizar producto");
+				  System.out.println("4. Agregar nuevo producto");
+				  System.out.println("5. Eliminar producto del sistema");
+				  System.out.println("6. Ver stock de producto");
+				  System.out.println("7. Calcular stock de producto");
+				  
+				  scanner.nextLine(); /* adicional, para consumir el salto de línea pendiente, que queda en el búfer de entrada.
+				   Prepara correctamente el búfer de entrada para que la siguiente llamado, evitando que el nextLine() del titulo, 
+				   lea ese salto de línea (y devuelva una cadena vacía) en lugar de la línea ingresada por el usuario (el título) */
+
+				  Proveedor completarConstructor = new Proveedor(0, null, 0, null);
+				  Producto ejecutarMetodosProductos = new Producto(0, null, null, 0, completarConstructor , 0);
+				  int opcionProductos = scanner.nextInt(); 
+				  
+			 switch (opcionProductos) {
+				 
+				  case 1:
+					  
+					  System.out.println("Elegiste la opcion de ver productos!");
+					  ejecutarMetodosProductos.Ver();
+					  
+					  break;
+				  case 2:
+					  
+					  System.out.println("Elegiste la opcion de buscar producto!");
+					  
+					  System.out.println("Ingrese el id del Producto:");
+					  int id = scanner.nextInt(); 
+					  ejecutarMetodosProductos.Buscar(id);
+					  
+					  break;
+				  case 3:
+					  
+					  System.out.println("Elegiste la opcion de actualizar producto!");
+					  ejecutarMetodosProductos.Actualizar(ejecutarMetodosProductos);
+					  
+					  break;
+				  case 4:
+					  
+					  System.out.println("Elegiste la opcion de agregar producto!");
+					  ejecutarMetodosProductos.Agregar();
+					  
+					  break;
+				  case 5:
+					  
+					  System.out.println("Elegiste la opcion de eliminar producto!");
+					  ejecutarMetodosProductos.Eliminar(ejecutarMetodosProductos);
+					  
+					  break;
+				  case 6:
+					  
+					  System.out.println("Elegiste la opcion de ver stock!");
+					  ejecutarMetodosProductos.verStock(ejecutarMetodosProductos);
+					  
+					  break;
+				  case 7:
+					  
+					  System.out.println("Elegiste la opcion de calcular stock!");
+					  ejecutarMetodosProductos.calcularStock(ejecutarMetodosProductos);
+					  
+					  break;
+			 }
+			  volverAlMenu(salir);
+			   break;
+			
+			case 4://VENTAS
+				break;
+				
+			case 5://GASTOS
+				break;
+				
+			case 0: //Salir
+	
+				System.out.println("Hasta Pronto!");	
+				salir.set(true);;
+				
+				break;	
+			default:
+				
+				System.out.println("Opcion inválida! Vuelva a intentarlo");
+				salir.set(false);
+			}
+		}
+
+	}
+		public static void volverAlMenu(AtomicBoolean salir) {
+	/*Utilizo AtomicBoolean para mantener el estado mutable de la variable salir. 
+	 * La función toma el AtomicBoolean como parámetro y puede modificar su valor utilizando el método set().*/
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		  System.out.println("Desea volver al menu (si/no)?");
+		  String volver = scanner.next();
+		  
+		  if(volver.equalsIgnoreCase("si")) {
+			  salir.set(false);
+			  
+		  }else {
+			  System.out.println("Hasta pronto!");
+			  salir.set(true);
+		  }
 	}
 
 }
