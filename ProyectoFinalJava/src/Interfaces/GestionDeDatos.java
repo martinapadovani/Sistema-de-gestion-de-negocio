3ad12e676461ae92a30bed3feaa3f9a6f4127137
Martina Padovani <133980405+martinapadovani@users.noreply.github.com>
2023-07-07 10:07:01 -0300

    Update GestionDeDatos.java

--- ProyectoFinalJava/src/Interfaces/GestionDeDatos.java
+++ ProyectoFinalJava/src/Interfaces/GestionDeDatos.java
@@ -4,9 +4,9 @@ import ClasesConcretas.Producto;
 
 public interface GestionDeDatos <T> {
 	
-	abstract void Agregar(T objeto);
+	abstract void Ver();
 	abstract void Buscar(int ID);
 	abstract void Actualizar(T objeto);
+	abstract void Agregar();
 	abstract void Eliminar(T objeto);
-
 }
