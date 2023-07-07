4b164a59b431bf6ae5b4e266d3b1e82498f39e88
Martina Padovani <133980405+martinapadovani@users.noreply.github.com>
2023-07-07 10:06:25 -0300

    Update Conexion.java
    Agregado de mis datos MySQL

--- ProyectoFinalJava/src/ConexionDB/Conexion.java
+++ ProyectoFinalJava/src/ConexionDB/Conexion.java
@@ -7,39 +7,37 @@ import java.sql.SQLException;
 public class Conexion {
 	
 
-	private final static String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
+ private final static String CONTROLADOR = "com.mysql.cj.jdbc.Driver";
 	private final static String URL = "jdbc:mysql://localhost:3306/java_proyecto";
 	private final static String USER = "root";
 	private final static String PASSWORD = "roukoperro2013";
-
+	private final static String PASSWORDM = "dimon2003";
 	
 	static {
 		try {
 			Class.forName(CONTROLADOR);
-
-		} catch(ClassNotFoundException e) {
+			
+			System.out.println("CONNECTED");
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error al cargar el controlador");
 			e.printStackTrace();
 		}
 	}
-	
-
 	
 	public Connection conectar() {
 		
 		Connection conexion = null;
 		
 		try {
-			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
-			
-			System.out.println("CONECTADO");
+			conexion = DriverManager.getConnection(URL, USER, PASSWORDM);
 			
 		} catch (SQLException e) {
 			System.out.println("Error en la conexión");
 			e.printStackTrace();
 		}
-	
+		
 		return conexion;
-
 	}
 
 }
